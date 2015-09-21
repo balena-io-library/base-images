b3abfa25fd4787e2ef4eeb0b254b3b49cadd2cde
James <james@resin.io>
2015-09-21 19:13:18 +0700

    Add support for TS-4900

--- python/generate-dockerfile.sh
+++ python/generate-dockerfile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-devices='raspberrypi raspberrypi2 beaglebone edison nuc vab820-quad zc702-zynq7 odroid-c1 odroid-ux3 parallella-hdmi-resin nitrogen6x cubox-i'
+devices='raspberrypi raspberrypi2 beaglebone edison nuc vab820-quad zc702-zynq7 odroid-c1 odroid-ux3 parallella-hdmi-resin nitrogen6x cubox-i ts4900'
 suites='jessie wheezy'
 
 for device in $devices; do
