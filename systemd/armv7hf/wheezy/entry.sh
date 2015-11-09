b6b3fa8f260ab3a3a1c8a4e05fe2238b9b85b914
Trong Nghia Nguyen <nghiant2710@gmail.com>
2015-11-09 21:40:07 +0100

    Add autogenerated Dockerfiles

--- systemd/armv7hf/wheezy/entry.sh
+++ systemd/armv7hf/wheezy/entry.sh
@@ -5,7 +5,19 @@ echo $HOSTNAME > /etc/hostname
 echo "127.0.1.1 $HOSTNAME" >> /etc/hosts
 hostname $HOSTNAME
 
-mount -t devtmpfs none /dev
+mkdir -p /tmp
+mount -t devtmpfs none /tmp
+mkdir -p /tmp/shm
+mount --move /dev/shm /tmp/shm
+mkdir -p /tmp/mqueue
+mount --move /dev/mqueue /tmp/mqueue
+mkdir -p /tmp/pts
+mount --move /dev/pts /tmp/pts
+touch /tmp/console
+mount --move /dev/console /tmp/console
+umount /dev || true
+mount --move /tmp /dev
+mount -t debugfs nodev /sys/kernel/debug
 udevd & 
 udevadm trigger &> /dev/null
 	
