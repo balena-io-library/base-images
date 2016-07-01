a5d2f61cc6a6dafe6e110867ebcecf95770983b1
Trong Nghia Nguyen <nghiant2710@gmail.com>
2016-07-01 13:26:22 +0700

    Add node base images for beaglebone-green-wifi

--- node/generate-latest-dockerfile.sh
+++ node/generate-latest-dockerfile.sh
@@ -36,7 +36,7 @@ C4F0DFFF4E8C1A8236409D08E73BC641CC11F4C8 \
 	gpg --keyserver ha.pool.sks-keyservers.net --recv-keys "$key"; \
 done
 
-devices='raspberrypi raspberrypi2 beaglebone edison nuc vab820-quad zc702-zynq7 odroid-c1 odroid-ux3 parallella-hdmi-resin nitrogen6x cubox-i ts4900 colibri-imx6 apalis-imx6 ts7700 raspberrypi3 artik5 artik10'
+devices='raspberrypi raspberrypi2 beaglebone edison nuc vab820-quad zc702-zynq7 odroid-c1 odroid-ux3 parallella-hdmi-resin nitrogen6x cubox-i ts4900 colibri-imx6 apalis-imx6 ts7700 raspberrypi3 artik5 artik10 beaglebone-green-wifi'
 nodeVersions='0.10.22 0.10.45 0.12.14 4.4.5 5.11.1 6.2.2'
 defaultVersion='0.10.22'
 resinUrl="http://resin-packages.s3.amazonaws.com/node/v\$NODE_VERSION/node-v\$NODE_VERSION-linux-#{TARGET_ARCH}.tar.gz"
@@ -58,6 +58,10 @@ for device in $devices; do
 			binary_arch='armv7hf'
 		;;
 		'beaglebone')
+			binary_url=$resinUrl
+			binary_arch='armv7hf'
+		;;
+		'beaglebone-green-wifi')
 			binary_url=$resinUrl
 			binary_arch='armv7hf'
 		;;
