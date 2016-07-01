0c415428f918d7c90c0e5ad3cec8cd98d03908da
Trong Nghia Nguyen <nghiant2710@gmail.com>
2016-07-01 10:22:13 +0200

    Autogenerated Dockerfiles

--- alpine/armhf/3.3/entry.sh
+++ alpine/armhf/3.3/entry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-HOSTNAME=$(cat /etc/hostname)-${RESIN_DEVICE_UUID:0:6}
+HOSTNAME=$HOSTNAME-${RESIN_DEVICE_UUID:0:6}
 echo $HOSTNAME > /etc/hostname
 echo "127.0.1.1 $HOSTNAME" >> /etc/hosts
 hostname $HOSTNAME
