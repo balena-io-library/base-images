c817e95ec26ba0f21a77ce6477d187a868a9053f
Trong Nghia Nguyen <nghiant2710@gmail.com>
2016-07-18 17:25:59 +0200

    Autogenerated Dockerfiles

--- debian/i386/jessie/entry.sh
+++ debian/i386/jessie/entry.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
 
-HOSTNAME=$HOSTNAME-${RESIN_DEVICE_UUID:0:6}
-echo $HOSTNAME > /etc/hostname
-echo "127.0.1.1 $HOSTNAME" >> /etc/hosts
-hostname $HOSTNAME
+function update_hostname()
+{
+	HOSTNAME=$HOSTNAME-${RESIN_DEVICE_UUID:0:6}
+	echo $HOSTNAME > /etc/hostname
+	echo "127.0.1.1 $HOSTNAME" >> /etc/hosts
+	hostname $HOSTNAME
+}
 
-mkdir -p /tmp
-mount -t devtmpfs none /tmp
-mkdir -p /tmp/shm
-mount --move /dev/shm /tmp/shm
-mkdir -p /tmp/mqueue
-mount --move /dev/mqueue /tmp/mqueue
-mkdir -p /tmp/pts
-mount --move /dev/pts /tmp/pts
-touch /tmp/console
-mount --move /dev/console /tmp/console
-umount /dev || true
-mount --move /tmp /dev
+function mount_dev()
+{
+	mkdir -p /tmp
+	mount -t devtmpfs none /tmp
+	mkdir -p /tmp/shm
+	mount --move /dev/shm /tmp/shm
+	mkdir -p /tmp/mqueue
+	mount --move /dev/mqueue /tmp/mqueue
+	mkdir -p /tmp/pts
+	mount --move /dev/pts /tmp/pts
+	touch /tmp/console
+	mount --move /dev/console /tmp/console
+	umount /dev || true
+	mount --move /tmp /dev
 
-# Since the devpts is mounted with -o newinstance by Docker, we need to make
-# /dev/ptmx point to its ptmx.
-# ref: https://www.kernel.org/doc/Documentation/filesystems/devpts.txt
-ln -sf /dev/pts/ptmx /dev/ptmx
+	# Since the devpts is mounted with -o newinstance by Docker, we need to make
+	# /dev/ptmx point to its ptmx.
+	# ref: https://www.kernel.org/doc/Documentation/filesystems/devpts.txt
+	ln -sf /dev/pts/ptmx /dev/ptmx
+	mount -t debugfs nodev /sys/kernel/debug
+}
 
-if [ "$INITSYSTEM" = "on" ]; then
+function init_systemd()
+{
 	GREEN='\033[0;32m'
 	echo -e "${GREEN}Systemd init system enabled."
 	env > /etc/docker.env
@@ -39,7 +47,10 @@ if [ "$INITSYSTEM" = "on" ]; then
 	EOF
 
 	exec /sbin/init quiet systemd.show_status=0
-else
+}
+
+function init_non_systemd()
+{
 	udevd & 
 	udevadm trigger &> /dev/null
 	
@@ -52,4 +63,16 @@ else
 		echo "Command not found: $1"
 		exit 1
 	fi
+}
+
+if [ ! -z "$RESIN_SUPERVISOR_API_KEY" ] && [ ! -z "$RESIN_DEVICE_UUID" ]; then
+	# run this on resin device only
+	update_hostname
+	mount_dev
+fi 
+
+if [ "$INITSYSTEM" = "on" ]; then
+	init_systemd $@
+else
+	init_non_systemd $@
 fi
