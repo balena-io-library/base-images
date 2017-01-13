26b4819c2e64155ecd0914f182dd2d4bb63337ee
Trong Nghia Nguyen <nghiant2710@gmail.com>
2017-01-13 11:55:12 +0700

    Set Alpine Linux 3.5 as the latest version for all Alpine Linux base images

--- openjdk/generate-dockerfile.sh
+++ openjdk/generate-dockerfile.sh
@@ -23,7 +23,7 @@ declare -A debCache=()
 versions='7-jdk 7-jre 8-jdk 8-jre'
 devices='raspberrypi raspberrypi2 beaglebone edison nuc vab820-quad zc702-zynq7 odroid-c1 odroid-ux3 parallella-hdmi-resin nitrogen6x cubox-i ts4900 colibri-imx6 apalis-imx6 ts7700 raspberrypi3 artik5 artik10 beaglebone-green-wifi qemux86 qemux86-64 beaglebone-green intel-quark artik710'
 fedora_devices=' raspberrypi2 beaglebone vab820-quad zc702-zynq7 odroid-c1 odroid-ux3 parallella-hdmi-resin nitrogen6x cubox-i ts4900 colibri-imx6 apalis-imx6 raspberrypi3 artik5 artik10 beaglebone-green-wifi beaglebone-green nuc qemux86-64 artik710 '
-alpineVersion='3.4'
+alpineVersion='3.5'
 alpineDeviceArchs='x86_64 x86 armhf'
 variants='jre jdk'
 
