25ad987c5aaaf0de94306cc5e21fb0bb56b860fc
Trong Nghia Nguyen <nghiant2710@gmail.com>
2018-03-14 11:19:35 +0700

    Upgrade mraa to v1.9.0 and upm to v1.6.0

--- device-base/generate-dockerfile.sh
+++ device-base/generate-dockerfile.sh
@@ -18,12 +18,10 @@ bb_key_cmd='apt-key adv --keyserver keyserver.ubuntu.com --recv-key D284E608A4C4
 bb_sourceslist_wheezy_cmd='echo "deb http://debian.beagleboard.org/packages wheezy-bbb main" >> /etc/apt/sources.list'
 bb_key_wheezy_cmd='apt-key adv --keyserver keyserver.ubuntu.com --recv-key B2710B8359890110'
 
-# intel-edison
-mraa_version='1.8.0'
-mraa_commit='eb7238d9afea044701dba1c26cc5076854e3238c'
-
-upm_version='1.5.0'
-upm_commit='b9010059ade9b3ee9fb343b23c506acaed7d0b15'
+mraa_version='1.9.0'
+mraa_commit='fbb7d9232067eac3f4508a37a8f7ea0c4fcebacb'
+upm_version='1.6.0'
+upm_commit='cc7fec9ae0228add9011bf1c2cd5e0ca2ba0d4f0'
 
 devices='raspberry-pi raspberry-pi2 beaglebone-black intel-edison intel-nuc via-vab820-quad zc702-zynq7 odroid-c1 odroid-xu4 parallella nitrogen6x hummingboard ts4900 colibri-imx6dl apalis-imx6q ts7700 raspberrypi3 artik5 artik10 beaglebone-green-wifi qemux86 qemux86-64 beaglebone-green cybertan-ze250 artik710 am571x-evm up-board kitra710 imx6ul-var-dart kitra520 jetson-tx2 iot2000 jetson-tx1'
 fedora_devices=' raspberry-pi2 beaglebone-black via-vab820-quad zc702-zynq7 odroid-c1 odroid-xu4 parallella nitrogen6x hummingboard ts4900 colibri-imx6dl apalis-imx6q raspberrypi3 artik5 artik10 beaglebone-green-wifi beaglebone-green intel-nuc qemux86-64 artik710 am571x-evm kitra710 up-board imx6ul-var-dart kitra520 jetson-tx2 jetson-tx1 '
@@ -75,7 +73,7 @@ for device in $devices; do
 		fedora_baseImage='armv7hf-fedora'
 	;;
 	'intel-edison')
-		template='Dockerfile.i386.edison.tpl'
+		template='Dockerfile.tpl'
 		baseImage='i386-debian'
 		# TODO: Can't compile mraa on alpine linux atm, lack of necessary libraries.
 		#alpine_template='Dockerfile.alpine.i386.intel-edison.tpl'
@@ -294,12 +292,6 @@ for device in $devices; do
 					-e "s@#{KEYS}@$key@g" \
 					-e "s@#{DEV_TYPE}@$device@g" $template > $debian_dockerfilePath/$suite/Dockerfile
 			;;
-			"intel-edison"|"up-board")
-				sed -e "s@#{MRAA_COMMIT}@$mraa_commit@g" \
-					-e "s@#{MRAA_VERSION}@$mraa_version@g" \
-					-e "s@#{UPM_COMMIT}@$upm_commit@g" \
-					-e "s@#{UPM_VERSION}@$upm_version@g" Dockerfile.mraa.partial >> $debian_dockerfilePath/$suite/Dockerfile
-			;;
 			"raspberry"*)
 				case "$suite" in
 				'buster')
@@ -324,6 +316,14 @@ for device in $devices; do
 				if [ $device == "iot2000" ] || [ $device == "cybertan-ze250" ]; then
 					cat Dockerfile.i386.nlp.partial >> $debian_dockerfilePath/$suite/Dockerfile
 				fi
+
+				if [ $device == "intel-edison" ] || [ $device == "up-board" ]; then
+					sed -e "s@#{MRAA_COMMIT}@$mraa_commit@g" \
+						-e "s@#{MRAA_VERSION}@$mraa_version@g" \
+						-e "s@#{UPM_COMMIT}@$upm_commit@g" \
+						-e "s@#{UPM_VERSION}@$upm_version@g" Dockerfile.mraa.partial >> $debian_dockerfilePath/$suite/Dockerfile
+				fi
+
 			;;
 			esac
 		done
