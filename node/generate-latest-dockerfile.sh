397be2a0e6ed3ac20cabca6285a3d84671c68ea2
Trong Nghia Nguyen <nghiant2710@gmail.com>
2018-05-23 11:42:55 +0700

    Add node v10.1.0 v8.11.2 and v6.14.2

--- node/generate-latest-dockerfile.sh
+++ node/generate-latest-dockerfile.sh
@@ -48,7 +48,7 @@ targets='raspberry-pi raspberry-pi2 beaglebone-black intel-edison intel-nuc via-
 # List of archs
 targets+=' armv7hf armel i386 amd64 aarch64'
 fedora_targets=' raspberry-pi2 beaglebone-black via-vab820-quad zynq-xz702 odroid-c1 odroid-xu4 parallella nitrogen6x hummingboard ts4900 colibri-imx6dl apalis-imx6q raspberrypi3 artik5 artik10 beaglebone-green-wifi beaglebone-green intel-nuc qemux86-64 artik710 am571x-evm kitra710 up-board imx6ul-var-dart kitra520 jetson-tx2 jetson-tx1 armv7hf amd64 aarch64 generic-aarch64 generic-armv7ahf bananapi-m1-plus orangepi-plus2 fincm3 '
-nodeVersions='0.10.22 0.10.48 0.12.18 5.12.0 7.10.1 9.11.1 8.11.1 6.14.1 4.9.1 10.0.0'
+nodeVersions='0.10.22 0.10.48 0.12.18 5.12.0 7.10.1 9.11.1 8.11.2 6.14.2 4.9.1 10.1.0'
 defaultVersion='0.10.22'
 yarnVersion='1.5.1'
 resinUrl="http://resin-packages.s3.amazonaws.com/node/v\$NODE_VERSION/node-v\$NODE_VERSION-linux-#{TARGET_ARCH}.tar.gz"
