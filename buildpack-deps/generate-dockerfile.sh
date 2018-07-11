ebeef535b5a1e5e4755277b464a174a60c0b870b
Trong Nghia Nguyen <nghiant2710@gmail.com>
2018-07-11 14:55:48 +0700

    Add Alpine Linux 3.8 buildpack-deps base images

--- buildpack-deps/generate-dockerfile.sh
+++ buildpack-deps/generate-dockerfile.sh
@@ -7,7 +7,7 @@ targets='raspberry-pi raspberry-pi2 beaglebone-black intel-edison intel-nuc via-
 targets+=' armv7hf armel i386 amd64 aarch64'
 fedora_targets=' raspberry-pi2 beaglebone-black via-vab820-quad zynq-xz702 odroid-c1 odroid-xu4 parallella nitrogen6x hummingboard ts4900 colibri-imx6dl apalis-imx6q raspberrypi3 artik5 artik10 beaglebone-green-wifi beaglebone-green intel-nuc qemux86-64 artik710 am571x-evm kitra710 up-board imx6ul-var-dart kitra520 jetson-tx2 jetson-tx1 armv7hf amd64 aarch64 generic-armv7ahf generic-aarch64 bananapi-m1-plus orangepi-plus2 fincm3 artik533s artik530 '
 suites='jessie stretch'
-alpine_suites='edge 3.7'
+alpine_suites='edge 3.8'
 fedora_suites='25 26'
 
 for target in $targets; do
