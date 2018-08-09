125550d27af7d63ea676fa9d258cc21c17d31ee6
Rich Bayliss <rich@resin.io>
2018-08-09 15:48:28 +0100

    Added support for devices & alpine

--- dotnet/generate-all-dockerfile.sh
+++ dotnet/generate-all-dockerfile.sh
@@ -3,9 +3,12 @@ set -e
 
 out_dir=.
 
-devices='raspberry-pi raspberry-pi2 raspberrypi3 beaglebone edison nuc vab820-quad zc702-zynq7 odroid-c1 odroid-ux3 parallella-hdmi-resin nitrogen6x cubox-i ts4900 colibri-imx6 apalis-imx6'
+# List of devices
+targets='raspberry-pi raspberry-pi2 beaglebone-black intel-edison intel-nuc via-vab820-quad zynq-xz702 odroid-c1 odroid-xu4 parallella nitrogen6x hummingboard ts4900 colibri-imx6dl apalis-imx6q ts7700 raspberrypi3 artik5 artik10 beaglebone-green-wifi qemux86 qemux86-64 beaglebone-green cybertan-ze250 artik710 am571x-evm up-board kitra710 imx6ul-var-dart kitra520 jetson-tx2 jetson-tx1 iot2000 generic-aarch64 generic-armv7ahf bananapi-m1-plus orangepi-plus2 fincm3 artik533s artik530 orbitty-tx2 spacely-tx2'
+# List of archs
+targets+=' armv7hf armel i386 amd64 aarch64'
 variants='sdk runtime aspnetcore'
-bases='debian'
+bases='debian:jessie alpine:3.7'
 
 OPTIND=1
 while getopts "o:" opt; do
@@ -17,53 +20,23 @@ while getopts "o:" opt; do
 done
 shift $((OPTIND-1))
 
-for device in $devices; do
-	case "$device" in
-	'raspberry-pi')
-		binary_arch='armv6hf'
-	;;
-	'raspberry-pi2' | 'raspberrypi3')
-		binary_arch='armv7hf'
-	;;
-	'beaglebone')
-		binary_arch='armv7hf'
-	;;
-	'edison')
-		binary_arch='x86'
-	;;
-	'nuc')
-		binary_arch='x64'
-	;;
-	'vab820-quad')
-		binary_arch='armv7hf'
-	;;
-	'zc702-zynq7')
-		binary_arch='armv7hf'
-	;;
-	'odroid-c1')
-		binary_arch='armv7hf'
-	;;
-	'odroid-ux3')
-		binary_arch='armv7hf'
-	;;
-	'parallella-hdmi-resin')
-		binary_arch='armv7hf'
-	;;
-	'nitrogen6x')
-		binary_arch='armv7hf'
-	;;
-	'cubox-i')
-		binary_arch='armv7hf'
-	;;
-	'ts4900')
-		binary_arch='armv7hf'
-	;;
-	'colibri-imx6')
-		binary_arch='armv7hf'
-	;;
-	'apalis-imx6')
-		binary_arch='armv7hf'
-	;;
+for target in $targets; do
+	case "$target" in
+    'raspberry-pi'|'armel')
+      binary_arch='armv6hf'
+    ;;
+    'intel-edison'|'qemux86'|'i386')
+      binary_arch='x86'
+    ;;
+    'intel-nuc'|'qemux86-64'|'amd64')
+      binary_arch='x64'
+    ;;
+    'generic-aarch64'|'aarch64')
+      binary_arch='aarch64'
+    ;;
+    *)
+      binary_arch='armv7hf'
+    ;;
 	esac
 
   case "$binary_arch" in
@@ -77,12 +50,27 @@ for device in $devices; do
       dotnet_arch='arm64'
     ;;
     *)
-      echo "Unsupported arch '$binary_arch'! Skipping device '$device'..."
+      echo "Unsupported arch '$binary_arch'! Skipping device '$target'..."
       continue
     ;;
   esac
 
   for base in $bases; do
+
+    IFS=: read -r base_image base_image_label <<< "$base"
+
+    case $dotnet_arch in
+        'arm'|'arm64')
+          crossbuild=''
+          ;;
+        *)
+          crossbuild='# '
+          case $base_image in
+            'alpine')
+              dotnet_arch='musl-x64'
+          esac
+          ;;
+      esac
 
     for variant in $variants; do
       case "$variant" in
@@ -95,15 +83,23 @@ for device in $devices; do
           version='2.1.2'
         ;;
         'aspnetcore')
+          case "$dotnet_arch" in
+            'arm64')
+              dotnet_arch='arm'
+              ;;
+          esac
           bundle="https://download.microsoft.com/download/1/f/7/1f7755c5-934d-4638-b89f-1f4ffa5afe89/aspnetcore-runtime-2.1.2-linux-$dotnet_arch.tar.gz"
           version='2.1.2'
         ;;
       esac
 
-      dockerfilePath=$out_dir/$device/$base/$variant-$version
+      bundle_basename=$(basename $bundle)
+      sha256sum=$(cat ./SHA256SUMS | grep -i $bundle_basename)
+
+      dockerfilePath=$out_dir/$target/$base_image/$version/$variant
       mkdir -p $dockerfilePath
-      sed -e s~#{FROM}~resin/$device-$base:jessie~g \
-        -e s~#{DOTNET_BUNDLE}~$bundle~g Dockerfile.tpl > $dockerfilePath/Dockerfile
+
+      sed "s~#{FROM}~resin/$target-$base_image:$base_image_label~g; s~#{DOTNET_BUNDLE}~$bundle~g; s~#{DOTNET_BUNDLE_BASENAME}~$bundle_basename~g; s~#{DOTNET_BUNDLE_SHA256SUM}~$sha256sum~g; s~#{ARCH=ARM}~$crossbuild~g" Dockerfile.$base_image.tpl > $dockerfilePath/Dockerfile
 
     done
   done
