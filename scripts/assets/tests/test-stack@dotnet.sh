8accad6af708fca7271c5c65f18a86782e19f877
Trong Nghia Nguyen <nghiant2710@gmail.com>
2019-09-10 23:30:26 +0700

    test: Add initial tests for balenalib base images
    Change-type: minor
    Signed-off-by: Trong Nghia Nguyen <nghiant2710@gmail.com>

---
+++ scripts/assets/tests/test-stack@dotnet.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+function test_hello_world {
+    local result
+
+    if [ -z ${DOTNET_SDK_VERSION+x} ]; then
+    	echo "No .NET hello world test, skipping..."; exit 0
+    else
+        if [ -z "$(dotnet --list-sdks)" ] ; then
+             echo ".NET SDK should be installed, and isn't." ; exit 1
+        fi
+        dotnet new console -o testApp
+        cd testApp
+        result=$(dotnet run)
+        cd ..
+        rm -rf testApp
+        [ "$result" == "Hello World!" ] || ( echo ".NET hello world test failed." ; exit 1 )
+        echo ".NET hello test passed!"
+    fi
+}
+
+function main {
+    test_hello_world
+}
+
+main
